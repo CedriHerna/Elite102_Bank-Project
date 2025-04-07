0066a9cea0744b0b32ff1a9f4408f9252f66c86e
Cedric Hernandez <cedricphernandez@gmail.com>
2025-04-07 18:03:00 -0500

    Just added psuedo code

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-# Hiii
+# First need to have a introduction of my Banking System.
+
+# Make a log in to the bank and tell the user what
+# type of access they have.
+
+# IF THE USER IS NOT AN ADMIN
+# Functions:
+
+# Accounts: 
+
+#   Manage account: 
+
+#       Check Balance
+
+#       Deposit
+
+#       Withdraw
+
+
+# IF THE USER IS AN ADMIN
+
+# Functions:
+
+# List of Users and their info
+
+# Modify / Close any account
+
+#       NAME | PIN | ADDRESS | SSN
+
+# Add account
